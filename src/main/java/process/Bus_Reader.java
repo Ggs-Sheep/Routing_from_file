50bbc604f630a23e33ef93c20e37fd64364700bc
clarence claux <claux59.c@gmail.com>
2022-05-10 16:51:30 +0200

    adding bus_reader

--- src/main/java/process/Bus_Reader.java
+++ src/main/java/process/Bus_Reader.java
@@ -1,11 +1,17 @@
-package Process;
+package process;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class Bus_Reader {
+    private ArrayList<String> stations;
+
+    public Bus_Reader() {
+        stations = new ArrayList<>();
+    }
 
     public void lectureFichier(){
         try {
@@ -13,11 +19,15 @@ public class Bus_Reader {
             ObjectMapper mapper = new ObjectMapper();
 
             // convert JSON file to map
-            Map<?, ?> map = mapper.readValue(Paths.get("book.json").toFile(), Map.class);
+            Map<?, ?> map = mapper.readValue(Paths.get("res/bus.json").toFile(), Map.class);
 
             // print map entries
+
+            System.out.println(map.get("horaires"));
+
             for (Map.Entry<?, ?> entry : map.entrySet()) {
-                System.out.println(entry.getKey() + "=" + entry.getValue());
+                //System.out.println(entry.getKey('horaires'));
+                //System.out.println(entry.getKey() + "=" + entry.getValue() + "\n");
             }
 
         } catch (Exception ex) {
