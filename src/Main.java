f83a84ca9a4d16bfb80087eeb90d9e5de6f3bd70
fAlcide <alcide.faucheron@etu.uphf.fr>
2022-05-10 15:35:05 +0200

    Suppression AllStations et initialisation du main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-import models.AllStations;
+import models.Ligne;
+import models.Station;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        AllStations stations = new AllStations();
+
+        ArrayList<Ligne> lignes = new ArrayList<Ligne>();
+        ArrayList<Station> stations = new ArrayList<Station>();
+
     }
 }
